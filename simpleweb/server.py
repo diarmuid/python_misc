0b39b0f5ce58f91762526d0d6319ab5ff6198d0d
diarmuid <diarmuid.m.collins@gmail.com>
2014-09-13 17:07:55 +0000

    added bottle server

---
+++ simpleweb/server.py
@@ -0,0 +1,8 @@
+from bottle import route, run, template
+import os
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=os.getenv('PORT', 8080))
